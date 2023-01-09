200f71585baf131092a9c2ea18b2a0162c42198e
raoole <espinaraul@gmail.com>
2023-01-09 07:10:41 -0400

    saved

--- Section 24/code/01-starting-project/src/App.js
+++ Section 24/code/01-starting-project/src/App.js
@@ -6,13 +6,25 @@ import Backdrop from "./components/Backdrop/Backdrop";
 import List from "./components/List/List";
 
 class App extends Component {
+  state = {
+    modalIsOpen: false
+  }
+
+  showModel = () => {
+    this.setState({ modalIsOpen: false })
+  }
+
+  closeModel = () => {
+    this.setState({ modalIsOpen: true })
+  }
+
   render() {
     return (
       <div className="App">
         <h1>React Animations</h1>
-        <Modal />
-        <Backdrop />
-        <button className="Button">Open Modal</button>
+        <Modal show={this.state.modalIsOpen} closed={this.closeModel} />
+        <Backdrop show={this.state.modalIsOpen} />
+        <button className="Button" onClick={this.showModel}>Open Modal</button>
         <h3>Animating Lists</h3>
         <List />
       </div>
--- Section 24/code/01-starting-project/src/components/Modal/Modal.js
+++ Section 24/code/01-starting-project/src/components/Modal/Modal.js
@@ -2,11 +2,17 @@ import React from 'react';
 
 import './Modal.css';
 
-const modal = (props) => (
+const modal = (props) => {
+
+
+    
+    const cssClasses = ['Modal', props.show ? 'Modal-open' : 'Modal-closed']
+    
+    return(
     <div className="Modal">
         <h1>A Modal</h1>
         <button className="Button" onClick={props.closed}>Dismiss</button>
     </div>
-);
+);}
 
 export default modal;
